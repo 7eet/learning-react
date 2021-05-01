10463454de05683d017dbfedb7c5bbb9c59cee9e
7eet <raisimranjeetsingh@gmail.com>
2021-05-01 17:39:54 +0530

    added counter project

---
+++ counter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './component/Counter';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App;
